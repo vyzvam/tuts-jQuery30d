ae174de9b37325d75326b6979a5aa91a3fd8e419
Subathiran Subramaniam <vyzvam@gmail.com>
2012-02-19 22:54:18 +0800

    1.0.2.21
    Integrated javascript's prototype to be used with the slider

--- lesson-14/slider.proto.js
+++ lesson-14/slider.proto.js
@@ -1,37 +1,27 @@
-(function($) {
+function Slider(container, nav) {
+	console.log(nav);
+	this.container = container;
+	this.nav       = nav.show();
+	this.imgs      = this.container.find('img');
+	this.imgWidth  = this.imgs[0].width;
+	this.current   = 0;
+}
 
-	var slideUL = $('div.slider').css({overflow: 'hidden'}).children('ul')
-		, imgs = slideUL.find('img')
-		, imgWidth = imgs[0].width
-		, imgsLength = imgs.length
-		, current = 0
+Slider.prototype.transition = function(position) {
+	
+	//make sure to provide a parameter to parse 
+	//to allow customization
+	this.container.animate ({
+		'margin-left': position || -(this.current * this.imgWidth)
+	});
+};
+
+Slider.prototype.setCurrent = function(direction) {
+	var pos = this.current
+	,   count = this.imgs.length 
 	;
 
+	pos += ~~(direction == "next") || -1;
+	this.current = (pos < 0 ) ? count - 1 : pos % count;
+};
 
-	$('div.slider_nav').show().find('button').on('click', function() {
-
-		var direction = $(this).data('dir')
-		;
-
-
-
-		current += ~~(direction == "next") || -1 ;
-		current = (current < 0) ? imgsLength -1: current % imgsLength;
-
-
-		console.log(current);
-
-		transition(slideUL, current * imgWidth , direction);
-	});
-
-	function transition(container, loc, direction) {
-
-		container.animate({'margin-left': -(loc)});
-
-	}
-
-
-
-
-
-})(jQuery);
